bacf9ccd7511b1c146c85d13e5f44a795aff0826
yangyangruirui <514303840@qq.com>
2023-01-03 16:29:09 +0800

    项目初始化-调整目录结构

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,29 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    root: true,
+
+    env: {
+        node: true
+    },
+
+    extends: [
+        'plugin:vue/essential',
+        '@vue/standard'
+    ],
+
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+
+    rules: {
+        'no-console': 'off',
+        'no-debugger': 'off',
+        'no-multiple-empty-lines': 0,
+        'space-before-function-paren': 0,
+        indent: 4
+    },
+
+    'extends': [
+      'plugin:vue/essential',
+      '@vue/standard'
+    ]
 }
